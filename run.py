09669e3617694b1bda16576031ea3fd111366719
Pete Bachant <petebachant@gmail.com>
2016-02-07 13:47:45 -0500

    Add args to run script

--- run.py
+++ run.py
@@ -19,14 +19,26 @@ def create_input_file(tsr=3.1, dynamic_stall=0):
 def run_cactus(tsr=3.1, overwrite=False, **kwargs):
     """Run CACTUS and write output to `cactus.log`."""
     if not os.path.isfile("cactus.log") or overwrite:
-        create_input_file(**kwargs)
+        create_input_file(tsr=tsr, **kwargs)
         if not os.path.isdir("results"):
             os.mkdir("results")
         os.chdir("results")
         call("cactus ../config/RM2.in | tee ../cactus.log", shell=True)
     else:
-        sys.exit("Simulation results present; use ./clean.sh to remove")
+        sys.exit("Simulation results present; use ./clean.sh to remove "
+                 "or -f to overwrite")
 
 
 if __name__ == "__main__":
-    run_cactus(tsr=3.1, dynamic_stall=0, overwrite=True)
+    import argparse
+    parser = argparse.ArgumentParser(description="Run CACTUS for the RM2.")
+    parser.add_argument("tsr", default=3.1, type=float, nargs="?",
+                        help="Tip speed ratio")
+    parser.add_argument("--dynamic-stall", "-d", default=0, type=int,
+                        help="Dynamic stall model", choices=[0, 1, 2])
+    parser.add_argument("--overwrite", "-f", default=False, action="store_true",
+                        help="Overwrite existing results")
+
+    args = parser.parse_args()
+    run_cactus(tsr=args.tsr, dynamic_stall=args.dynamic_stall,
+               overwrite=args.overwrite)
