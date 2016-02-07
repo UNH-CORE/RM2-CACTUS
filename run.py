d38926f4ceebd8ec50b5e31e04644657c0b731b8
Pete Bachant <petebachant@gmail.com>
2016-02-07 13:14:54 -0500

    Add run_cactus function

--- run.py
+++ run.py
@@ -6,10 +6,17 @@ import os
 import sys
 
 
-if not os.path.isfile("cactus.log"):
-    if not os.path.isdir("results"):
-        os.mkdir("results")
-    os.chdir("results")
-    call("cactus ../config/RM2.in | tee ../cactus.log", shell=True)
-else:
-    sys.exit("Simulation results present; use ./clean.sh to remove")
+def run_cactus(overwrite=False):
+    """Run CACTUS and write output to `cactus.log`."""
+    if not os.path.isfile("cactus.log") or overwrite:
+        if not os.path.isdir("results"):
+            os.mkdir("results")
+        os.chdir("results")
+        call("cactus ../config/RM2.in | tee ../cactus.log", shell=True)
+    else:
+        sys.exit("Simulation results present; use ./clean.sh to remove")
+
+
+if __name__ == "__main__":
+    overwrite = True
+    run_cactus(overwrite=overwrite)
