d13247937bf018ae55ab5dee0f20301e10ede51b
Pete Bachant <petebachant@gmail.com>
2016-02-07 12:38:21 -0500

    Modify geom script to work with Octave

--- scripts/RM2_Geom.m
+++ scripts/RM2_Geom.m
@@ -4,12 +4,12 @@ close all
 % Creates test VAWT geometry file
 
 % Add geom creation scripts to path
-path(path,'Z:\CACTUS\Rel1_1\CreateGeom');
+path(path,'~/CACTUS/CACTUS-tools/geometry/CreateGeom');
 
 % Params
 R = 0.5375*3.28084;         % Center radius (ft)
 R_m = 0.5375;               % radius in m at midspan
-HR = 0.807/R_m;             % Height to radius ratio 
+HR = 0.807/R_m;             % Height to radius ratio
 eta = 0.5;                  % Blade mount point ratio (mount point behind leading edge as a fraction of chord)
 NBlade = 3;                 % number of blades
 NBElem = 20;                % number of blade elements
@@ -27,16 +27,17 @@ CRr(1,((NBElem/2)+1):(NBElem+1)) = CRr_1;
 
 
 % Output filename
-FN = 'RM2_Geom.geom';
+FN = 'config/geom/RM2_Geom.geom';
 
 % Plot data?
-PlotTurbine = 1;
+PlotTurbine = 0;
 
 % Convert
 dToR = pi/180;
 FlipN = 1;
 
 % Create basic parabolic blade VAWT
+% CreateTurbine(NBlade,NBElem,NStrut,NSElem,RefR,RotN,RotP,RefAR,Type,varargin)
 Type = 'VAWT';
 BShape = 0;
 T = CreateTurbine(NBlade,NBElem,NStrut,NSElem,R,[],[],[],Type,1,CRr,HR,eta,BShape,CRs,TCs);
@@ -46,30 +47,30 @@ WriteTurbineGeom(FN,T);
 
 % Plot if desired
 if PlotTurbine
-    
+
     % Plot animated turbine rotation
     XLim=[-2,2];
     YLim=[0,2];
     ZLim=[-2,2];
-    
+
     % Plot controls
     PlotVec=0;
     SFVec=.5;
     Trans=.5;
-    
+
     hf=figure(1);
-    set(hf,'Position',[303   124   956   610]) 
+    set(hf,'Position',[303   124   956   610])
     set(gca,'Position',[5.2743e-002  5.1245e-002  8.9979e-001  8.8141e-001])
     set(gca,'CameraPosition',[-52.1999   30.4749   62.2119])
     set(gca,'CameraUpVector',[1.8643e-001  9.7433e-001 -1.2615e-001])
     set(gca,'CameraViewAngle',6.3060e+000)
     grid on
     set(gcf,'Color','white');
-    hl=light('Position',[-1,0,0]);
+    % hl=light('Position',[-1,0,0]); % Not compatible with octave
     set(gca,'Color','white');
     set(gca,'DataAspectRatio',[1,1,1])
     set(gca,'XLim',XLim,'YLim',YLim,'ZLim',ZLim)
-    
+
     HIn=[];
     PhasePlot=linspace(0,2*pi,150);
     for i=1:length(PhasePlot)
@@ -77,7 +78,5 @@ if PlotTurbine
        HIn=H;
        pause(.01);
     end
-    
+
 end
-
-
