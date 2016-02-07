168879fb0ad05710a303c071d3faf1c8476f5820
Pete Bachant <petebachant@gmail.com>
2016-02-07 13:27:52 -0500

    Use template for input file

--- run.py
+++ run.py
@@ -6,6 +6,16 @@ import os
 import sys
 
 
+def create_input_file(tsr=3.1, dynamic_stall=0):
+    """Create CACTUS input file `config/RM2.in`."""
+    params = {"dynamic_stall": dynamic_stall,
+              "tsr": tsr}
+    with open("config/RM2.in.template") as f:
+        txt = f.read()
+    with open("config/RM2.in", "w") as f:
+        f.write(txt.format(**params))
+
+
 def run_cactus(overwrite=False):
     """Run CACTUS and write output to `cactus.log`."""
     if not os.path.isfile("cactus.log") or overwrite:
@@ -19,4 +29,5 @@ def run_cactus(overwrite=False):
 
 if __name__ == "__main__":
     overwrite = True
+    create_input_file(tsr=3.1)
     run_cactus(overwrite=overwrite)
