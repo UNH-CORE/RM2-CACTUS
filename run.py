9c350362c43fc673068cbb75b0a1e236c655b5d1
Pete Bachant <petebachant@gmail.com>
2016-02-07 17:43:56 -0500

    Properly change directories in run.py

--- run.py
+++ run.py
@@ -28,10 +28,10 @@ def run_cactus(tsr=3.1, overwrite=False, **kwargs):
         create_input_file(tsr=tsr, **kwargs)
         if not os.path.isdir("results"):
             os.mkdir("results")
-        root_dir = os.path.split(__file__)[0]
+        start_dir = os.getcwd()
         os.chdir("results")
         call("cactus ../config/RM2.in | tee ../cactus.log", shell=True)
-        os.chdir(root_dir)
+        os.chdir(start_dir)
     else:
         sys.exit("Simulation results present; use ./clean.sh to remove "
                  "or -f to overwrite")
