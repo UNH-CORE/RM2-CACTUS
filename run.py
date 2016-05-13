c1951dbc47824846960f591f9e65f6961b03c3e6
Pete Bachant <petebachant@gmail.com>
2016-05-13 09:10:46 -0400

    Change u_infty arg name

--- run.py
+++ run.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
                         help="Run parameter sweep [name] [start] [stop] [step]")
     parser.add_argument("--dynamic-stall", "-d", default=2, type=int,
                         help="Dynamic stall model", choices=[0, 1, 2])
-    parser.add_argument("--u-infty", "-U", type=float, default=1.0,
+    parser.add_argument("--u_infty", "-U", type=float, default=1.0,
                         help="Free stream velocity in m/s")
     parser.add_argument("--nti", "-n", type=int, default=24,
                         help="Time steps per rev")
