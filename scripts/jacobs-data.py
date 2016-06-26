75fe3ad87bb742656b474c83a37ee8a23dd98a61
Pete Bachant <petebachant@gmail.com>
2016-06-26 12:34:02 -0400

    Do not recalculate BV stall angles by default

--- scripts/jacobs-data.py
+++ scripts/jacobs-data.py
@@ -6,6 +6,14 @@ import numpy as np
 import pandas as pd
 
 
+# Default CACTUS 0021 data has zero for all moment coeffs
+zero_cms = True
+# Whether or not to calculate LB DS model critical lift coefficients
+calc_lb_crit_cls = True
+# Whether or not to calculate BV DS model stall angles
+calc_bv_stall_angles = False
+
+
 header = \
 """Title: NACA0021
 Thickness to Chord Ratio: 0.21
@@ -22,13 +30,46 @@ LB Dyn. Stall Model - Negative Critical Lift Coeff.: {lb_neg_crit_cl}
 AOA (deg) CL CD Cm25"""
 
 re_list = ["8.3e4", "1.6e5", "3.8e5"]
-
+# Default parameters from CACTUS input data (all but lift slopes are replaced)
 bv_stall_angles = {"8.3e4": 4.0, "1.6e5": 5.0, "3.8e5": 5.0}
 lb_lift_slopes = {"8.3e4": 5.277, "1.6e5": 5.371, "3.8e5": 6.303}
 lb_crit_cls = {"8.3e4": 0.829, "1.6e5": 1.031, "3.8e5": 1.32}
 
+
+def detect_ss_angle(df, threshold=0.03):
+    """Detect static stall angle from input DataFrame by finding where the
+    change in `cd` per degree `alpha_deg` reaches `threshold`.
+
+    Should be run on the Sheldahl data since Jacobs does not contain `cd`.
+    """
+    df["alpha"] = df.alpha_deg
+    df = df[df.alpha > 2]
+    df = df[df.alpha < 40]
+    dcd_dalpha = np.diff(df.cd) / np.diff(df.alpha)
+    i = np.where(dcd_dalpha >= threshold)[0][0]
+    return df.alpha.iloc[i]
+
+
+def calc_crit_cl(df, re, fcrit=0.7, alpha1_fraction=0.87):
+    """Calculate critical lift coefficient for Leishman--Beddoes model.
+
+    Code from turbinesFoam:
+
+        CN1_ = CNAlpha_*alpha1_*pow((1.0 + sqrt(f))/2.0, 2);
+
+    Technically this returns the critical normal force coefficient.
+    """
+    df["alpha_rad"] = np.deg2rad(df.alpha_deg)
+    alpha1 = np.deg2rad(alpha1_fraction*detect_ss_angle(df))
+    # Use default lift slopes from CACTUS data
+    cn_alpha = lb_lift_slopes[re]
+    cn1 = cn_alpha*alpha1*((1.0 + np.sqrt(fcrit))/2.0)**2
+    return cn1
+
+
 # Create empty dictionary for DataFrames
 dfs = {}
+
 
 # Load Jacobs data and mirror about zero angle of attack
 for re in re_list:
@@ -40,6 +81,7 @@ for re in re_list:
     df["alpha_deg"] = alpha
     df["cl"] = cl
     dfs[re] = df
+
 
 # Fill in Jacobs C_d and C_m data from Sheldahl, interpolating to the Jacobs
 # AoAs
@@ -54,6 +96,13 @@ for re in re_list:
     df = df_sh_save_neg.append(df, ignore_index=True)
     df = df.append(df_sh_save_pos, ignore_index=True)
     dfs[re] = df
+    # Calculate critical normal force coefficients and use as critical `cl`
+    if calc_lb_crit_cls:
+        lb_crit_cls[re] = calc_crit_cl(df_sh, re)
+    # Detect static stall angles for BV model
+    if calc_bv_stall_angles:
+        bv_stall_angles[re] = detect_ss_angle(df_sh)
+
 
 # Write final text file in correct format
 txt = header + "\n\n"
@@ -64,6 +113,8 @@ for re in re_list:
                             lb_pos_crit_cl=lb_crit_cls[re],
                             lb_neg_crit_cl=lb_crit_cls[re]) + "\n"
     df = dfs[re]
+    if zero_cms:
+        df.cm *= 0.0
     for alpha_deg, cl, cd, cm in zip(df.alpha_deg, df.cl, df.cd, df.cm):
         txt += str(alpha_deg) + "\t" + str(cl) + "\t" + str(cd) + "\t" + str(cm)
         txt += "\n"
